a6660c76574cd51c34ba57649de29ab7c09e9411
Dzbice <dechawnz@gmail.com>
2026-01-22 22:56:23 -0500

    added function, testing crawl

--- crawlandscrape.py
+++ crawlandscrape.py
@@ -2,7 +2,16 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 
+
+baseUrl = "https://www.sciencealert.com"
 years = ["2022","2023","2024","2025","2026"]
+months = ["01","02","03","04","05","06","07","08","09","10","11","12"]
+for i in years:
+    for j in months:
+        req = requests.get(baseUrl + "/"+i+"/"+j)
+        s = BeautifulSoup(req.text,'html.parser')
+        print(s.find("section",class_="col-span-containter"))
+        
 res = requests.get("https://www.sciencealert.com/suni-williams-retires-after-record-space-career-and-final-ill-fated-mission")
 id = 23675
 
