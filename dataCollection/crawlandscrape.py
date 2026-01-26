b1d4435b8df34dba74bb0162d94a04d35eebb161
Dzbice <dechawnz@gmail.com>
2026-01-26 14:07:59 -0500

    crawling and scraping created, publisher list added

--- dataCollection/crawlandscrape.py
+++ dataCollection/crawlandscrape.py
@@ -1,44 +1,85 @@
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
-import urllib
+from urllib.parse import urlparse
 
 
-baseUrl = "https://www.sciencealert.com"
-years = ["2022","2023","2024","2025","2026"]
-months = ["01","02","03","04","05","06","07","08","09","10","11","12"]
-for i in years:
-    for j in months:
-        req = requests.get(baseUrl + "/"+i+"/"+j)
-        s = BeautifulSoup(req.text,'html.parser')
-        cont = s.find("div", class_="container-posts")
-        if cont:
-            print(f"{i}/{j}")
-            link = cont.find_all("a")
-        else:
-            pass
-x = 0
-for _ in link:
-    print(_['href'])
-    x = x+1
-    print(x)
+def loadPublisherDomans(path):
+    df = pd.read_csv(path)
+    return set(df["Publisher Domain"].tolist())
 
+def crawlto(i, j=""):
+    req = requests.get(baseUrl + "/"+i+"/"+j)
+    s = BeautifulSoup(req.text,'html.parser')
+    cont = s.find("div", class_="container-posts")
+    if cont:
+        link = cont.find_all("a")
+        for k in link:
+            saveSite(k['href'])
 
-
-res = requests.get("https://www.sciencealert.com/suni-williams-retires-after-record-space-career-and-final-ill-fated-mission")
-id = 23675
-
-publisher_domains = []
+def crawl():
+    for i in years:
+        if i =="2026":
+            crawlto("2026")
+            break
+        for j in months:
+            crawlto(i,j)
+            
 
 def saveSite(site):
+    global totalSites, totalLinks, publishedLinks, id
+    totalSites = totalSites+1
     res = requests.get(site)
     soup = BeautifulSoup(res.text, 'html.parser')
     content = soup.find("section", class_="col-span-container")
     content_links = content.find_all("a")
+    publisherLinksFound = []
     for i in content_links:
-        for j in publisher_domains:
-            if j in i:
-                pass
+        totalLinks = totalLinks+1
+        href = i.get("href")
+        if not href:
+            continue
+        if urlparse(href).netloc in publisher_domains:
+            publisherLinksFound.append(href)
+    if publisherLinksFound:
+        publishedLinks = publishedLinks + len(publisherLinksFound)
+        saveInfo(content, site, publishedLinks)
+        print(publisherLinksFound)
+        id = id +1
 
-def saveInfo():
-    pass
+
+def saveInfo(content,site, links):
+    TITLE = content.find("h1",class_="text-h2 sm:text-h1 font-source-sans font-semibold text-legibility block mb-4").text
+    AUTHORS = content.find("span", class_=" text-body-sm sm:text-body-md uppercase inline").text
+    CATEGORY = content.find("a",class_="article-cat text-body-sm sm:text-body-md uppercase font-source-sans font-semibold text-legibility inline-block mr-3").text
+    DATE = content.find("span",class_="article-date text-body-xs sm:text-body-sm text-gray-500 block mr-3").text
+    PLAINTEXT = content.get_text()
+    df.loc[len(df)] = {
+    "id": id,
+    "title": TITLE,
+    "authors": AUTHORS,
+    "category": CATEGORY,
+    "date": DATE,
+    "url": site,
+    "plaintext": PLAINTEXT,
+    "html": str(content),
+    "all_links": links
+}
+
+
+df = pd.DataFrame()
+publisher_domains = loadPublisherDomans("dataCollection/Publishers_Domain_List.csv")
+baseUrl = "https://www.sciencealert.com"
+years = ["2022","2023","2024","2025","2026"]
+months = ["01","02","03","04","05","06","07","08","09","10","11","12"]
+res = requests.get("https://www.sciencealert.com/suni-williams-retires-after-record-space-career-and-final-ill-fated-mission")
+id = 23675
+totalSites = 0
+totalLinks = 0
+publishedLinks = 0
+
+crawl()
+df.to_csv("22-0126.csv")
+with open("stats.txt","w") as fp:
+    fp.write(f"totalSites: {totalSites}\n totalLinks: {totalLinks}\n publishedLinks: {publishedLinks}")
+
